15ef9c8d66c624ea41bfe528079ad1f117141e8e
Miniiar Rakhimkulov <miniyar23@gmail.com>
2024-01-07 21:32:46 +0100

    models + migrations

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Product, Review
+
 
 # Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'brand',
+        'category',
+        'price',
+        'countInStock',
+        'rating',
+        'numReviews',
+    )
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'product',
+        'user',
+        'name',
+        'rating',
+        'comment',
+    )
+
--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=255)
+    # image = models.ImageField(upload_to='products/', null=True, blank=True)
+    brand = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    description = models.TextField()
+    rating = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    numReviews = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    countInStock = models.IntegerField(default=0)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.rating)
